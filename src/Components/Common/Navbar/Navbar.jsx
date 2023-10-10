043b2a4ec141596707e0605485b100d7cee62b87
Moein <moeinganji2004@gmail.com>
2023-10-10 22:46:32 +0330

    Navbar

--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -1,44 +1,45 @@
 import IconBahr from "./../../../Assets/Images/Group 4.png";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="w-full h-28 border-2  flex items-center ">
       <div className="font-semibold text-xl flex justify-end">
-        <a
+        <NavLink
           href="#"
           className="bg-bluePrimary text-white rounded-full py-2 px-6 mr-2"
         >
           ورود
-        </a>
-        <a
+        </NavLink>
+        <NavLink
           href="#"
           className=" text-bluePrimary rounded-full py-1 px-5 border-2 border-solid border-bluePrimary"
         >
           ثبت نام
-        </a>
+        </NavLink>
       </div>
       <div className="w-6/12 h-16 border-2  text-center font-semibold text-xl flex items-center justify-center flex-row-reverse mx-auto">
-        <a
+        <NavLink
           href="#"
           className="px-1 py-1 text-bluePrimary border-b-4 rounded-sm border-solid border-bluePrimary ml-6"
         >
           خانه
-        </a>
-        <a href="#" className="px-1 py-1 ml-6">
+        </NavLink>
+        <NavLink href="#" className="px-1 py-1 ml-6">
           دوره ها
-        </a>
-        <a href="#" className="px-1 py-1 ml-6">
+        </NavLink>
+        <NavLink href="#" className="px-1 py-1 ml-6">
           اساتید
-        </a>
-        <a href="#" className="px-1 py-1 ml-6">
+        </NavLink>
+        <NavLink href="#" className="px-1 py-1 ml-6">
           اخبار مقالات
-        </a>
-        <a href="#" className="px-1 py-1 ml-6">
+        </NavLink>
+        <NavLink href="#" className="px-1 py-1 ml-6">
           درباره ما
-        </a>
-        <a href="#" className="px-1 py-1 ml-6">
+        </NavLink>
+        <NavLink href="#" className="px-1 py-1 ml-6">
           تماس با ما
-        </a>
+        </NavLink>
       </div>
       <div>
         <div className="w-36 h-16 flex mb-4">
