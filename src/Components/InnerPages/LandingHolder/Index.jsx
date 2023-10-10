730dc21a110790d569b78d2d3a3f2d59be004d51
Moein <moeinganji2004@gmail.com>
2023-10-10 14:26:00 +0330

    ServicesSection

--- src/Components/InnerPages/LandingHolder/Index.jsx
+++ src/Components/InnerPages/LandingHolder/Index.jsx
@@ -1,10 +1,12 @@
 import { LandingHistorySection } from "./LandingHistorySection/LandingHistorySection";
 import { LandingPopularCoursesSection } from "./LandingPopularCoursesSection/LandingPopularCoursesSection";
+import { ServicesSection } from "./LandingServicesSection/ServicesSection";
 import { LandingTeachersSection } from "./LandingTeachersSection/LandingTeachersSection";
 
 const LandingHolder = () => {
   return (
     <>
+      <ServicesSection />
       <LandingPopularCoursesSection />
       <LandingHistorySection />
       <LandingTeachersSection />
--- src/Components/InnerPages/LandingHolder/LandingServicesSection/ServicesSection.jsx
+++ src/Components/InnerPages/LandingHolder/LandingServicesSection/ServicesSection.jsx
@@ -2,10 +2,64 @@ import { LandingSectionsTitle } from "../../../Common/LandingSectionsTitle/Landi
 
 const ServicesSection = () => {
   return (
-    <LandingSectionsTitle
-      mainText="دسته بندی ها"
-      subText="گستره وسیعی از موضوعات رو می توانید انتخاب کنید"
-    />
+    <div className="w-full width-handler">
+      <LandingSectionsTitle
+        mainText="خدمات ما"
+        subText="شما عزیزان می توانید از خدمات ما استفاده کنید"
+      />
+      <div className="w-full h-[280px] border-2 border-solid border-transparent flex items-center justify-center gap-7 mt-6 mb-6">
+        <div className="w-[200px] h-[270px] border-2 border-solid border-transparent rounded-[16px] bg-white shadow-lg">
+          <div className="text-[#575757] text-center">
+            <i className="fi fi-rr-diploma text-8xl text-textBlack block mt-6"></i>
+            <h1 className="text-textBlack font-extrabold text-[22px] mb-3">
+              مدرک معتبر
+            </h1>
+            <p className="text-[16px]">
+              مشاوران ما شما عزیزان
+              <br />
+              را راهنمایی می کنند
+            </p>
+          </div>
+        </div>
+        <div className="w-[200px] h-[270px] border-2 border-solid border-transparent rounded-[16px] bg-white shadow-lg">
+          <div className="text-[#575757] text-center">
+            <i className="fi-rr-messages-question text-8xl text-textBlack block mt-6"></i>
+            <h1 className="text-textBlack font-extrabold text-[22px] mb-3">
+              مشاوره
+            </h1>
+            <p className="text-[16px]">
+              مشاوران ما شما عزیزان
+              <br />
+              را راهنمایی می کنند
+            </p>
+          </div>
+        </div>
+        <div className="w-[200px] h-[270px] border-2 border-solid border-transparent rounded-[16px] bg-white shadow-lg">
+          <div className="text-[#575757] text-center">
+            <i className="fi fi-rr-test text-8xl text-textBlack block mt-6"></i>
+            <h1 className="text-textBlack font-extrabold text-[22px] mb-3">
+              آزمون ها
+            </h1>
+            <p className="text-[16px]">
+              مشاوران ما شما عزیزان
+              <br />
+              را راهنمایی می کنند
+            </p>
+          </div>
+        </div>
+        <div className="w-[200px] h-[270px] border-2 border-solid border-transparent rounded-[16px] bg-bluePrimary shadow-lg">
+          <div className="text-center text-white">
+            <i className="fi fi-rr-laptop-code text-8xl text-white block mt-6"></i>
+            <h1 className=" font-extrabold text-[22px] mb-3">ورود به بازار</h1>
+            <p className="text-[16px]">
+              مشاوران ما شما عزیزان
+              <br />
+              را راهنمایی می کنند
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
