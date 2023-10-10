c1c16e9e44301f14a362d5857969ec795b7011d6
Moein <moeinganji2004@gmail.com>
2023-10-10 08:45:41 +0330

    Services

---
+++ src/Components/InnerPages/LandingHolder/LandingServicesSection/ServicesSection.jsx
@@ -0,0 +1,12 @@
+import { LandingSectionsTitle } from "../../../Common/LandingSectionsTitle/LandingSectionsTitle";
+
+const ServicesSection = () => {
+  return (
+    <LandingSectionsTitle
+      mainText="دسته بندی ها"
+      subText="گستره وسیعی از موضوعات رو می توانید انتخاب کنید"
+    />
+  );
+};
+
+export { ServicesSection };
