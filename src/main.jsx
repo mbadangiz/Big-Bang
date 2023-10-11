d6b3a7c4663cf7b7d1ebd7aa47427cdde69a6aea
BehradMsv <behradmsv.bm@gmail.com>
2023-10-11 20:23:44 +0330

    Done with the Feature/Footer

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App/App";
 import "./index.css";
 import "./imp.css";
-import { Footer } from "./Components/Common/Footer";
-import { FooterLeaflet } from "./Components/Common/Footer/FooterMap/FooterLeaflet";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Footer />
+    <App />
   </React.StrictMode>
 );
