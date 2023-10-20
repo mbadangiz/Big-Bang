913644d587a06b1e7081c56e23a398725e3eac15
BehradMsv <behradmsv.bm@gmail.com>
2023-10-20 22:03:35 +0330

    Done with the Updating Blue Button Style

--- src/Components/Common/Buttons/BlueButton.jsx
+++ src/Components/Common/Buttons/BlueButton.jsx
@@ -4,8 +4,8 @@ const BlueButton = ({ type, buttonText, isDisabled }) => {
       type={type}
       className={
         isDisabled
-          ? "btn bg-gradient-to-t opacity-50 block mx-auto my-3 w-[120px] h-[44px] rounded-full text-[22px] text-white shadow-md pb-2  hover:shadow-inner hover:shadow-blue-900 transform hover:scale-125 transition ease-out duration-300"
-          : "btn bg-gradient-to-t from-bluePrimary to-blueSecondary block mx-auto my-3 w-[120px] h-[44px] rounded-full text-[22px] text-white shadow-md pb-2  hover:shadow-inner hover:shadow-blue-900 transform hover:scale-125 transition ease-out duration-300"
+          ? "btn bg-gradient-to-t opacity-50 block mx-auto w-[120px] h-[44px] rounded-full text-[22px] text-white shadow-md pb-2  hover:shadow-inner hover:shadow-blue-900 transform hover:scale-125 transition ease-out duration-300"
+          : "btn bg-gradient-to-t from-bluePrimary to-blueSecondary block mx-auto w-[120px] h-[44px] rounded-full text-[22px] text-white shadow-md pb-2  hover:shadow-inner hover:shadow-blue-900 transform hover:scale-125 transition ease-out duration-300"
       }
       disabled={isDisabled}
     >
