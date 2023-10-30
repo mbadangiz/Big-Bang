e519a559d7bca0f52d60b9db3b3f3801ead39836
BehradMsv <behradmsv.bm@gmail.com>
2023-10-31 01:51:28 +0330

    Done with the User/Panel/ChangePassword page

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -14,6 +14,7 @@ import { ForgetpassHolder } from "../Components/User/LoginRegisterForgetpass/For
 import { ForgetpassChangepassHolder } from "../Components/User/LoginRegisterForgetpass/ForgetpassHolder/ForgetpassChangepassHolder/index";
 import { PanelHolder } from "../Components/User/Panel/PanelHolder";
 import { PanelDashboard } from "../Components/User/Panel/PanelDashboard/PanelDashboard";
+import { PanelChangePassword } from "../Components/User/Panel/PanelChangePassword";
 
 function App() {
   const router = createBrowserRouter([
@@ -49,6 +50,10 @@ function App() {
       element: <PanelHolder />,
       children: [
         { path: "/User/Panel/Dashboard", element: <PanelDashboard /> },
+        {
+          path: "/User/Panel/ChangePassword",
+          element: <PanelChangePassword />,
+        },
       ],
     },
     { path: "/test", element: <Test /> },
