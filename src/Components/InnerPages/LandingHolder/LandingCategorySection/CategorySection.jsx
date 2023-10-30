a302bd1173230f0fb9314818ee1760ec9519fbed
Mohammad Badangiz <mbz3a1998@gmail.com>
2023-10-31 00:41:36 +0330

    Category Edit is done

--- src/Components/InnerPages/LandingHolder/LandingCategorySection/CategorySection.jsx
+++ src/Components/InnerPages/LandingHolder/LandingCategorySection/CategorySection.jsx
@@ -3,43 +3,40 @@ import Style from "./CategorySection.module.css";
 
 const CategorySection = () => {
   return (
-    <div className="w-full width-handler">
+    <div
+      className={`${Style.CategorySectionBackground}  width-handler h-screen px-0 py-10`}
+    >
       <LandingSectionsTitle
         mainText="دسته بندی ها"
         subText="گستره وسیعی از موضوعات رو می توانید انتخاب کنید"
       />
       <div
-        className={`${Style.CategorySectionBackground} w-full h-[806px] CategorySectionBackground flex gap-4 flex-wrap items-center justify-center relative`}
+        className={`w-full  flex gap-4 flex-wrap items-center justify-center relative my-8`}
       >
-        <div
-          className={`w-[250px] h-[320px] border-2 border-solid border-gray-200 bg-white rounded-[16px] text-center text-bluePrimary relative top-48`}
-        >
-          <i className="fi fi-br-database text-10xl block mt-5"></i>
-          <h1 className="text-4xl">دیتابیس</h1>
+        <CategoryCards icon={"fi fi-rs-pencil-paintbrush"} text={"UI/UX"} />
+        <div className="w-[30%] flex-row-all-center gap-6">
+          <CategoryCards icon={"fi fi-br-database"} text={"پایگاه داده"} />
+          <CategoryCards icon={"fi fi-rr-binary-lock"} text={"هک و امنیت"} />
+          <CategoryCards icon={"fi fi-rr-sitemap"} text={"شبکه"} />
+          <CategoryCards icon={"fi fi-rr-display-code"} text={"فرانت اند"} />
         </div>
-        <div className="w-[250px] h-[320px] border-2 border-solid border-gray-200 bg-white rounded-[16px] text-center text-bluePrimary">
-          <i className="fi fi-br-database text-10xl block mt-5"></i>
-          <h1 className="text-4xl">فرانت اند</h1>
-        </div>
-        <div className="w-[250px] h-[320px] border-2 border-solid border-gray-200 bg-white rounded-[16px] text-center text-bluePrimary">
-          <i className="fi fi-br-database text-10xl block mt-5"></i>
-          <h1 className="text-4xl">بک اند</h1>
-        </div>
-        <div className="w-[250px] h-[320px] border-2 border-solid border-gray-200 bg-white rounded-[16px] text-center text-bluePrimary relative top-48">
-          <i className="fi fi-br-database text-10xl block mt-5"></i>
-          <h1 className="text-4xl">UI & UX</h1>
-        </div>
-        <div className="w-[250px] h-[320px] border-2 border-solid border-gray-200 bg-white rounded-[16px] text-center text-bluePrimary">
-          <i className="fi fi-br-database text-10xl block mt-5"></i>
-          <h1 className="text-4xl">امنیت و هک</h1>
-        </div>
-        <div className="w-[250px] h-[320px] border-2 border-solid border-gray-200 bg-bluePrimary text-white rounded-[16px] text-center text-bluePrimary">
-          <i className="fi fi-br-database text-10xl block mt-5"></i>
-          <h1 className="text-4xl">شبکه</h1>
-        </div>
+        <CategoryCards icon={"fi fi-ss-square-terminal"} text={"بک اند"} />
       </div>
     </div>
   );
 };
 
 export { CategorySection };
+
+const CategoryCards = ({ text, icon, extraClasses }) => {
+  return (
+    <div
+      className={`flex-col-all-center w-[170px] h-[200px] border-2 border-solid border-gray-200
+       hover:bg-bluePrimary text-bluePrimary  rounded-[16px] text-center bg-white
+       hover:text-white cursor-pointer gap-5 transition-all duration-100 hover:translate-y-[-8px] ${extraClasses}`}
+    >
+      <i className={`${icon} text-7xl block mt-5`}></i>
+      <h1 className="text-xl f-bold">{text}</h1>
+    </div>
+  );
+};
