dd18cc04e185f480d0f4cb5560bba3e4946692ec
Moein <moeinganji2004@gmail.com>
2023-10-31 12:57:23 +0330

    edit Courses

--- src/Components/InnerPages/CoursesHolder/CoursesPage/InputSearch/InputSearch.jsx
+++ src/Components/InnerPages/CoursesHolder/CoursesPage/InputSearch/InputSearch.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 const InputSearch = () => {
   return (
-    <div className="flex relative">
+    <div className="flex relative m-10">
       <input
         type="search"
         className="border-2 border-solid border-white py-2 px-5 w-[500px]
         block m-auto rounded-2xl mt-3 mb-4 bg-slate-300/40"
         placeholder="جست و جو کنید..."
       ></input>
-      <i className="fi fi-rr-search"></i>
     </div>
   );
 };
