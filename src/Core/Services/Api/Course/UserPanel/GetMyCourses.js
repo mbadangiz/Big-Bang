6eecc9126cc850699d61d25b501b38b5cc9b2dae
Moein <moeinganji2004@gmail.com>
2023-11-22 20:28:03 +0330

    MyCourses Pane

---
+++ src/Core/Services/Api/Course/UserPanel/GetMyCourses.js
@@ -0,0 +1,14 @@
+import instance from "../../../Interceptor/Interceptor";
+
+const getMyCourses = async () => {
+  try {
+    const result = await instance.get(
+      `/SharePanel/GetMyCourses?PageNumber=1&RowsOfPage=10&SortingCol=DESC&SortType=LastUpdate&Query=`
+    );
+    return result;
+  } catch (err) {
+    console.log(err.response);
+    return [];
+  }
+};
+export { getMyCourses };
