0eaf839cd996f2cbf116bdaddbd13e9b24073e3d
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2024-11-29 23:06:39 +0330

    fix blog detail break words

--- src/Components/InnerPages/ArticleAndNewsDetailHolder/ArticleMainContents/ArticleTextContets/ArticleTextContets.jsx
+++ src/Components/InnerPages/ArticleAndNewsDetailHolder/ArticleMainContents/ArticleTextContets/ArticleTextContets.jsx
@@ -2,12 +2,14 @@ export const ArticleTextContets = ({ data }) => {
   const { googleTitle, miniDescribe, describe } = data.detailsNewsDto;
   return (
     <div>
-      <h2 className="text-lg f-bold my-4">{googleTitle}</h2>
-      <p className="text-sm text-justify leading-6 opacity-70 mb-5">
+      <h2 className="text-lg f-bold my-4 break-words">{googleTitle}</h2>
+      <p className="text-sm text-justify leading-6 opacity-70 mb-5 break-words">
         {miniDescribe}...
       </p>
       <div className="w-full mb-10">
-        <p className="text-sm text-justify leading-7 opacity-90">{describe}</p>
+        <p className="text-sm text-justify leading-7 opacity-90 break-words">
+          {describe}
+        </p>
       </div>
     </div>
   );
