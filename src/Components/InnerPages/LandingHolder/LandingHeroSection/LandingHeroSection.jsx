468e69068c8467583954007df1cb725bd2ee07c9
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2024-12-01 22:56:06 +0330

    fix margin hero section

--- src/Components/InnerPages/LandingHolder/LandingHeroSection/LandingHeroSection.jsx
+++ src/Components/InnerPages/LandingHolder/LandingHeroSection/LandingHeroSection.jsx
@@ -4,7 +4,7 @@ import { HeroSectionLeftSide } from "./HeroSectionLeftSide";
 
 const LandingHeroSection = () => {
   return (
-    <div className="width-handler mt-10 flex-row-all-center justify-between ">
+    <div className="width-handler mt-20 md:mt-10 flex-row-all-center justify-between ">
       <HeroSectionRightSide />
       <HeroSectionLeftSide />
     </div>
